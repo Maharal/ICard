b135cdd9081dd52d1298cb909c0b2a6f496dd667
VictorHSMoura <victorhugosmoura@gmail.com>
2021-01-18 16:59:14 -0300

    Add signup tests and correct wrong tests

--- django_icard/login/tests.py
+++ django_icard/login/tests.py
@@ -29,14 +29,38 @@ class LoginTest(TestCase):
 
 class SignupTest(TestCase):
 
+    def test_signup_page_has_first_name_fild(self):
+        request = HttpRequest()
+        response = signup(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="text" class="form-control" id="id_first_name" name="first_name" placeholder=" ">', html)
+
+    def test_signup_page_has_last_name_fild(self):
+        request = HttpRequest()
+        response = signup(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="text" class="form-control" id="id_last_name" name="last_name" placeholder=" ">', html)
+
+    def test_signup_page_has_username_fild(self):
+        request = HttpRequest()
+        response = signup(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="text" class="form-control" id="id_last_name" name="last_name" placeholder=" ">', html)
+
     def test_signup_page_has_email_fild(self):
         request = HttpRequest()
         response = signup(request)
         html = response.content.decode('utf8')
-        self.assertIn('<input type="email" class="form-control" id="email" name="email" placeholder=" ">', html)
+        self.assertIn('<input type="email" class="form-control" id="id_email" name="email" placeholder=" ">', html)
 
     def test_signup_page_has_password_fild(self):
         request = HttpRequest()
         response = signup(request)
         html = response.content.decode('utf8')
-        self.assertIn('<input type="password" class="form-control" id="password" name="password" minlength="8" placeholder=" ">', html)
+        self.assertIn('<input type="password" class="form-control" id="id_password1" name="password1" minlength="8" placeholder=" ">', html)
+
+    def test_signup_page_has_password_confirmation_fild(self):
+        request = HttpRequest()
+        response = signup(request)
+        html = response.content.decode('utf8')
+        self.assertIn('<input type="password" class="form-control" id="id_password2" name="password2" minlength="8" placeholder=" ">', html)
