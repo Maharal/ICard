669b46519a3cfebab1979863e47b80d6d98da1ab
guilhermealbm <gui.miranda.13@gmail.com>
2021-01-18 14:56:27 -0300

    custom form to signup with first and lastname

--- django_icard/login/views.py
+++ django_icard/login/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+
+from .forms import SignUpForm
 
 # Create your views here.
 def home_page(request):
@@ -11,10 +15,21 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
