7f442c808636a66b8113a0d144f7048bae5274e9
guilhermealbm <gui.miranda.13@gmail.com>
2021-02-06 23:14:51 -0300

    add initial profile view

--- django_icard/login/urls.py
+++ django_icard/login/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
-from .views import signup
+from .views import signup, profile
 
 
 urlpatterns = [
     url(r'^signup/$', signup, name='signup'),
+    url(r'^profile/$', profile, name='profile'),
 ]
--- django_icard/login/views.py
+++ django_icard/login/views.py
@@ -23,3 +23,6 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def profile(request):
+    return render(request, 'profile.html')
