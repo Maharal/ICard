9d38a2c0c04790eb2d24b99e68988dcc71f601f8
guilhermealbm <gui.miranda.13@gmail.com>
2021-03-22 16:45:09 -0300

    delete profile

--- django_icard/cards/urls.py
+++ django_icard/cards/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
-from .views import edit_card, home_page, signup, profile, get_card, get_all_cards, card
+from .views import edit_card, home_page, signup, profile, delete_profile, get_card, get_all_cards, card
 
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
     url(r'^signup/$', signup, name='signup'),
     url(r'^profile/(?P<user_id>\d+)/$', profile, name='profile'),
+    url(r'^delete_profile/(?P<user_id>\d+)/$', delete_profile, name='delete_profile'),
     url(r'^get_card/$', get_card, name='get_card'),
     url(r'^get_all_cards/$', get_all_cards, name='get_all_cards'),
     url(r'^card/(?P<card_id>\d+)/$', card, name='card'),
