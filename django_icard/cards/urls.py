e6227b2cac8f6a8d394a2386a18d823bfcd6c7c6
VictorHSMoura <victorhugosmoura@gmail.com>
2021-03-22 17:13:46 -0300

    Favorite cards route

--- django_icard/cards/urls.py
+++ django_icard/cards/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from .views import edit_card, home_page, signup, profile, delete_profile, get_card, get_all_cards, card
+from .views import edit_card, home_page, signup, profile, delete_profile, get_card, get_all_cards, card, favorite_cards
 
 
 urlpatterns = [
@@ -11,4 +11,5 @@ urlpatterns = [
     url(r'^get_all_cards/$', get_all_cards, name='get_all_cards'),
     url(r'^card/(?P<card_id>\d+)/$', card, name='card'),
     url(r'^edit_card/(?P<card_id>\d+)/$', edit_card, name='edit_card'),
+    url(r'^favorite_cards/$', favorite_cards, name='favorite_cards'),
 ]
